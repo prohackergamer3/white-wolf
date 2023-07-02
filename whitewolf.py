3e8e3619a5450a683f840defb6d02879446ed2c2
PRO HACKER GAMER <109979523+prohackergamer3@users.noreply.github.com>
2023-07-02 14:21:19 +0300

    whitewolf.py

--- whitewolf.py
+++ whitewolf.py
@@ -1,5 +1,8 @@
 import os
-os.system("title WHİTE WOLF")
+import colorama
+import platform
+if platform.platform == "Windows":
+   os.system("title WHİTE WOLF")
 import wikipedia
 print("""  
 ░██╗░░░░░░░██╗██╗░░██╗██╗████████╗███████╗
@@ -15,7 +18,13 @@ print("""
 ░░████╔═████║░██║░░██║██║░░░░░██╔══╝░░
 ░░╚██╔╝░╚██╔╝░╚█████╔╝███████╗██║░░░░░
 ░░░╚═╝░░░╚═╝░░░╚════╝░╚══════╝╚═╝░░░░░""")
-ser = input("Ara:")
 wikipedia.set_lang("Tr")
-ss = wikipedia.summary(ser)
-print(ss)
+while True:
+       try:
+           ser = input("Ara:")
+           ss = wikipedia.summary(ser)
+           print(colorama.Fore.GREEN+ss)
+       except:
+           print(colorama.Fore.RED+"BÖYLE BİR ŞEY YOK"+colorama.Fore.GREEN)
+        
+        
